51fa5c405d2ffba05c89f70aab3335b07ecfd501
Artem Cherniy <artemcherniy7@gmail.com>
2022-01-19 10:53:45 +0000

    refactor

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Angular - The Complete Guide (2021 Edition)
 
 ## Created by Maximilian Schwarzmüller
+
 <img src="https://i.ibb.co/whGLvYy/Angular2021.png" alt="author">
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.5.
 
 Master Angular 12 (formerly "Angular 2") and build awesome, reactive web apps with the successor of Angular.js
+
 <hr>
 
 Check it online: https://angular-course-3605b.web.app
@@ -29,11 +31,16 @@ Check it online: https://angular-course-3605b.web.app
 <hr>
 
 ## To start
+
 - Clone file to your local machine `git clone https://github.com/artemchernii/Angular-Complete-Guide-2021-Edition.git`
 - Install all install all dependencies `npm install`. (In case you don't have the NodeJS installed please first check https://nodejs.org/en/)
 - If you don't have Angular CLI please first install it with this npm command `npm install -g @angular/cli`.
 - Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
 ## Tests
+
+<hr/>
 <p>
     For now I didn't make any tests for this project. Wait for updates.
 </p>
+ps Angular is fun
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,43 +1,46 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpParams} from '@angular/common/http';
-import {exhaustMap, map, mergeMap, take, tap} from 'rxjs/operators';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { exhaustMap, map, mergeMap, take, tap } from 'rxjs/operators';
 
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
-import {AuthService} from "../auth/auth.service";
+import { AuthService } from '../auth/auth.service';
 import { interval, Observable } from 'rxjs';
 import 'rxjs/add/operator/flatMap';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
   public testOb: Observable<any>;
-  constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) {}
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService,
+    private authService: AuthService
+  ) {}
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
     this.http
-      .put(
-        'https://angular-course-3605b.firebaseio.com/recipes.json',
-        recipes
-      )
-      .subscribe(response => {
+      .put('https://angular-course-3605b.firebaseio.com/recipes.json', recipes)
+      .subscribe((response) => {
         console.log(response);
       });
   }
 
   fetchRecipes() {
-    return this.http.get<Recipe[]>(
-      'https://angular-course-3605b.firebaseio.com/recipes.json'
-    ).pipe(map(recipes => {
-        return recipes.map(recipe => {
-          return {
-            ...recipe,
-            ingredients: recipe.ingredients ? recipe.ingredients : []
-          };
-        });
-      }),
-      tap(recipes => {
-        this.recipeService.setRecipes(recipes);
-      }));
+    return this.http
+      .get<Recipe[]>('https://angular-course-3605b.firebaseio.com/recipes.json')
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        }),
+        tap((recipes) => {
+          this.recipeService.setRecipes(recipes);
+        })
+      );
   }
 }
